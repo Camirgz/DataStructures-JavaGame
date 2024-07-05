4ca4f09d57b673cd31e992c0e24dc8d7143bb59a
Camila <camila.rodriguez.aguila@gmail.com>
2024-07-05 14:04:43 -0700

    Asegurarse que el arbol o la lista hayan sido creado

--- main.java
+++ main.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        //Incializaciones
+
+        //Inicializar 
         Scanner scanner = new Scanner(System.in);
-        MatrizDeJuego matriz = null;  // Inicializa matriz como nulo al inicio
-        ListaDobleEnlazada lista = new ListaDobleEnlazada();
-        ArbolBinario arbol = new ArbolBinario();
+        //Se pone en null para verificar luego que se hayan creado 
+        MatrizDeJuego matriz = null;
+        ListaDobleEnlazada lista = null;
+        ArbolBinario arbol = null;
 
-        System.out.println("\n=== Bienvenid@ ===");
+        System.out.println("\n === Bienvenid@ ===");
 
+        //Menu1
         while (true) {
-            System.out.println( "\n== Menu de opciones== \nIngrese el número correspondiente a su eleccion: \n");
-            System.out.println("1.Crear/cargar - Guardar matriz");
-            System.out.println("2.Crear Lista Enlazada Doble");
-            System.out.println("3.Crear Arbol Binario");
-            System.out.println("4.Operaciones con la Lista Enlazada Doble");
-            System.out.println("5.Operaciones con el Arbol Binario");
-            System.out.println("6.Salir");
+            System.out.println("\nMenu de opciones, \nIngrese el número correspondiente a tu elección: \n");
+            System.out.println("1. Crear/cargar - Guardar matriz");
+            System.out.println("2. Crear Lista Enlazada Doble");
+            System.out.println("3. Crear Arbol Binario");
+            System.out.println("4. Operaciones con la Lista Enlazada Doble");
+            System.out.println("5. Operaciones con el Arbol Binario");
+            System.out.println("6. Salir");
             int opcion = scanner.nextInt();
 
             try {
@@ -45,6 +48,7 @@ public class main {
                         if (matriz == null) {
                             System.out.println("Debe crear primero la matriz.");
                         } else {
+                            lista = new ListaDobleEnlazada();
                             Elemento[][] elementos = matriz.getMatriz();
                             for (Elemento[] fila : elementos) {
                                 for (Elemento e : fila) {
@@ -60,6 +64,7 @@ public class main {
                         if (matriz == null) {
                             System.out.println("Debe crear primero la matriz.");
                         } else {
+                            arbol = new ArbolBinario();
                             Elemento[][] elementos = matriz.getMatriz();
                             for (Elemento[] fila : elementos) {
                                 for (Elemento e : fila) {
@@ -75,19 +80,78 @@ public class main {
                         if (lista == null) {
                             System.out.println("Debe crear primero la lista enlazada.");
                         } else {
-                            // Operaciones con lista enlazada
+                            System.out.println("Operaciones con la Lista Enlazada Doble:");
+                            System.out.println("1. Obtener el elemento mAs grande");
+                            System.out.println("2. Obtener el elemento mAs pequeño");
+                            System.out.println("3. Obtener el promedio");
+                            System.out.println("4. Obtener cuAntos números primos tiene la lista");
+                            System.out.println("5. Guardar cada elemento de la lista en un archivo BINARIO");
+                            System.out.println("6. Cargar los elementos de un archivo binario en una nueva lista");
+                            int subOpcion4 = scanner.nextInt();
+                            switch (subOpcion4) {
+                                case 1:
+                                    System.out.println("Mayor elemento: " + lista.obtenerMayor());
+                                    break;
+                                case 2:
+                                    System.out.println("Menor elemento: " + lista.obtenerMenor());
+                                    break;
+                                case 3:
+                                    System.out.println("Promedio: " + lista.obtenerPromedio());
+                                    break;
+                                case 4:
+                                    System.out.println("Cantidad de números primos: " + lista.contarPrimos());
+                                    break;
+                                case 5:
+                                    lista.guardarEnBinario("lista.bin");
+                                    System.out.println("Lista guardada en lista.bin");
+                                    break;
+                                case 6:
+                                    lista.cargarDeBinario("lista.bin");
+                                    System.out.println("Lista cargada desde lista.bin");
+                                    lista.mostrar();
+                                    break;
+                                default:
+                                    System.out.println("Opción no vAlida.");
+                            }
                         }
                         break;
                     case 5:
                         if (arbol == null) {
                             System.out.println("Debe crear primero el Arbol binario.");
                         } else {
-                            // Operaciones con Arbol binario
+                            System.out.println("Operaciones con el Arbol Binario:");
+                            System.out.println("1. Obtener cantidad de nodos a la izquierda de la raíz del Arbol");
+                            System.out.println("2. Obtener cantidad de nodos a la derecha de la raíz del Arbol");
+                            System.out.println("3. Guardar cada recorrido del Arbol en archivos de texto");
+                            System.out.println("4. Cargar los datos del archivo recorrido PreOrden");
+                            int subOpcion5 = scanner.nextInt();
+                            switch (subOpcion5) {
+                                case 1:
+                                    System.out.println("Nodos a la izquierda: " + arbol.contarNodosIzquierda());
+                                    break;
+                                case 2:
+                                    System.out.println("Nodos a la derecha: " + arbol.contarNodosDerecha());
+                                    break;
+                                case 3:
+                                    arbol.guardarEnArchivo("arbol_preOrden.txt", "PreOrden");
+                                    arbol.guardarEnArchivo("arbol_Orden.txt", "Orden");
+                                    arbol.guardarEnArchivo("arbol_postOrden.txt", "PostOrden");
+                                    System.out.println("Recorridos guardados en archivos de texto.");
+                                    break;
+                                case 4:
+                                    arbol.cargarPreOrden("arbol_preorden.txt");
+                                    arbol.mostrarPreOrden();
+                                    arbol.mostrarOrden();
+                                    arbol.mostrarPostOrden();
+                                    break;
+                                default:
+                                    System.out.println("Opción no vAlida.");
+                            }
                         }
                         break;
                     case 6:
                         System.out.println("Gracias!");
-                        System.exit(0); //Salir del programa
+                        System.exit(0); // Salir del programa
                     default:
                         System.out.println("Opción no vAlida.");
                 }
